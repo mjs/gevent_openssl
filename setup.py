4c52c20d6159da89ca9bbaea650a3b215db4ce6a
Owen Raccuglia <ojracc@gmail.com>
2014-08-11 16:37:59 -0400

    Add setup.py for pip install
    This lets users install directly from github with pip, either by running
    it directly:
    
        pip install -e git+https://github.com/REPO/gevent_openssl#egg=gevent_openssl
    
    ...or by adding -e git+https://github.com/REPO/gevent_openssl#egg=gevent_openssl
    to a requirements file.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='gevent_openssl',
+      version='1.0',
+      description='A gevent wrapper for pyOpenSSL',
+      author='Phus Lu',
+      author_email='phus.lu@gmail.com',
+      url='https://github.com/phuslu/gevent_openssl',
+      packages=find_packages(),
+      zip_safe=False,
+      include_package_data=True,
+      license='New BSD',
+      platforms='any',
+      install_requires=['pyOpenSSL>=0.11', 'gevent'])
